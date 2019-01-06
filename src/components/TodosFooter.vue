42f9f3946f5d9bbae4e3644f11e35d8aeaee93c9
Oz <ozannethomas@hotmail.fr>
2019-01-06 23:37:20 +0100

    Finis de tout ajouter

---
+++ src/components/TodosFooter.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+
+<el-row :gutter="5">
+  <el-col :span="5">
+      <p class="taskStyleFooter"> <strong>{{store.length}} tâches </strong> à faire</p>
+  </el-col>
+  <el-col :span="5">
+        <el-button type="primary" @click.native="aFaire"  plain>A Faire</el-button>
+  </el-col>
+   <el-col :span="5">
+       <el-button type="success"  @click.native="faite" plain>Faite</el-button>
+  </el-col>
+  
+   <el-col :span="5">
+       <el-button type="danger" @click.native="deleteTacheFinis" splain>Supprimer les tâches finies</el-button>
+  </el-col>
+   
+</el-row>
+
+
+    </div>
+</template>
+
+
+
+<script>
+
+export default {
+    name:"todosFooter",
+    props: ['store'] ,
+   data: function () {
+  return {
+    
+  }
+},
+methods: 
+{
+  aFaire () {
+      this.$emit('aFaire')
+  },
+  faite () {
+      this.$emit('faite')
+  },
+  deleteTacheFinis() {
+      this.$emit('deleteTacheFinis')
+  }
+
+}
+}
+
+
+
+</script>
+
+
+<style>
+.taskStyleFooter{
+  font-family: "Helvetica Neue";
+  font-size: 15px;
+}
+</style>
